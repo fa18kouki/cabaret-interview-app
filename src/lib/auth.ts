0c31a5893894afcaa4cf2c5d246c46e523324177
Kanno Kouki <kouki0180@outlook.jp>
2026-02-01 20:33:43 +0900

    chore: remove Google and Twilio login
    - Remove Google OAuth provider from NextAuth config
    - Remove Google login button from SocialLoginButtons
    - Remove AUTH_GOOGLE_* and Twilio env vars from .env.example
    - Update auth tests (3 providers: LINE, X, Email)

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,6 +1,5 @@
 import NextAuth from "next-auth";
 import { PrismaAdapter } from "@auth/prisma-adapter";
-import Google from "next-auth/providers/google";
 import Line from "next-auth/providers/line";
 import Twitter from "next-auth/providers/twitter";
 import Nodemailer from "next-auth/providers/nodemailer";
@@ -9,10 +8,6 @@ import { prisma } from "@/server/db";
 export const { handlers, signIn, signOut, auth } = NextAuth({
   adapter: PrismaAdapter(prisma),
   providers: [
-    Google({
-      clientId: process.env.AUTH_GOOGLE_ID,
-      clientSecret: process.env.AUTH_GOOGLE_SECRET,
-    }),
     Line({
       clientId: process.env.AUTH_LINE_ID,
       clientSecret: process.env.AUTH_LINE_SECRET,
